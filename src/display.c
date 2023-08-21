115c02ebadc64f049cc886c364519176acbd0570
Fabiola de las Casas Escardó <fabioladelascasas@gmail.com>
2023-08-21 20:21:26 -0300

    Completo la tarea de nodesTask agregando el calculo del promedio de temperatura y humedad de cada nivel.

--- src/display.c
+++ src/display.c
@@ -16,18 +16,17 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define DISPLAY_TASK_DELAY      100
+#define DISPLAY_TASK_DELAY  100
 
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
 
-
 /*******************************************************************************
  * Variables
  ******************************************************************************/
-QueueHandle_t sensorData_queue;
 easyNex_t myNextion;
+
 /*******************************************************************************
  * Code - private
  ******************************************************************************/
@@ -37,28 +36,16 @@ easyNex_t myNextion;
  ******************************************************************************/
 void displayTask(void *arg)
 {
-    sensorData_queue = xQueueCreate(10, sizeof(protocol_frame_t));
-    
-    if(sensorData_queue == NULL)
-    {
-        while(1);
-    }
+    protocol_frame_t frame;
 
     EasyNex_begin(&myNextion, HMI_PORT, HMI_BAUDRATE);
 
     while (1) 
     {
         EasyNex_NextionListen(&myNextion);  //Lo uso para limpiar el buffer de la UART
-        // xQueueReceive(temperatureQueue, &(saunaHMI.temperature), 0);
+        //xQueueReceive(tempData_queue, &frame, 0);
 
         //refreshScreen();
-        // Serial.println("Estado pantalla:");
-        // Serial.printf("Pagina actual: %d \n", myNex.currentPageId);
-        // Serial.printf("Modo de luz: %d \n", (uint32_t)saunaHMI.lights);
-
-        vTaskDelay(20 / portTICK_PERIOD_MS);
-
-
 
         vTaskDelay(DISPLAY_TASK_DELAY / portTICK_PERIOD_MS);
     }
--- src/nodes.c
+++ src/nodes.c
@@ -24,14 +24,20 @@
  * Prototypes
  ******************************************************************************/
 static void hardware_config(void);
-
+static void calcular_PromedioNivel(uint16_t *temperatura, uint16_t *hum, protocol_frame_t node1, 
+                                                protocol_frame_t node2, protocol_frame_t node3);
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 static uint8_t manager_address = 0x00;
 
+uint32_t temp_silo[CANT_SILOS][CANT_NIVELES];
+uint32_t hum_silo[CANT_SILOS][CANT_NIVELES];
+
 QueueHandle_t node_uart_queue;
-extern QueueHandle_t sensorData_queue;
+QueueHandle_t humData_queue;
+QueueHandle_t tempData_queue;
+
 /*******************************************************************************
  * Code - private
  ******************************************************************************/
@@ -42,23 +48,55 @@ static void hardware_config(void)
     RS485_ConfigGPIO();
 }
 
+static void calcular_PromedioNivel(uint16_t *temperatura, uint16_t *hum, protocol_frame_t node1, 
+                                                protocol_frame_t node2, protocol_frame_t node3)
+{
+    uint32_t num1, num2, num3;
+
+    uint16_t mascara_temp = 0xFFFF0000;
+    
+    num1 = (uint16_t)(node1.payload & mascara_temp);
+    num2 = (uint16_t)(node2.payload & mascara_temp);
+    num3 = (uint16_t)(node3.payload & mascara_temp);
+
+    *temperatura = (num1 + num2 + num3)/3;      
+
+    num1 = (uint16_t)(node1.payload >> 16);
+    num2 = (uint16_t)(node2.payload >> 16);
+    num3 = (uint16_t)(node3.payload >> 16);
+
+    *hum = (num1 + num2 + num3)/3;      
+}
+
 /*******************************************************************************
  * Code - public
  ******************************************************************************/
-
 void nodeManagerTask(void *arg)
 {
+    uint8_t i;
     node_number_t node;
     uint16_t len_data = 0;
     uart_event_t event;
     int uart_read_flag = 0;
 
     protocol_frame_t frame_send;
-    protocol_frame_t frame_receive;
+    protocol_frame_t frame_receive[TOTAL_NODES];
     char data_send[NODE_BUF_SIZE];
     char data_receive[NODE_BUF_SIZE];
 
     hardware_config();
+
+    humData_queue = xQueueCreate(10, sizeof(uint32_t));
+    if(humData_queue == NULL)
+    {
+        while(1);
+    }
+
+    tempData_queue = xQueueCreate(10, sizeof(uint32_t));   
+    if(tempData_queue == NULL)
+    {
+        while(1);
+    }
 
     while (1) 
     {
@@ -85,14 +123,23 @@ void nodeManagerTask(void *arg)
 
                 if(UART_FAIL != uart_read_flag)
                 {
-                    protocol_readFrame(data_receive, &frame_receive);
+                    protocol_readFrame(data_receive, &frame_receive[node]);
                 }
-            }
 
-            if( xQueueSendToBack( sensorData_queue, ( void * ) &frame_receive, ( TickType_t ) 1000 ) != pdPASS )
-            {
-                /* Failed to post the message, even after 1000 ticks. */
+                memset(data_receive, 0, NODE_BUF_SIZE);      // Reset buffer
             }
+        }
+
+        node = 0;
+
+        for(i=1; i<(CANT_NIVELES+1) ; i++)
+        {
+            calcular_PromedioNivel(&temp_silo[CANT_SILOS][i], &hum_silo[CANT_SILOS][i],
+                                    frame_receive[node], frame_receive[node+CANT_NIVELES], frame_receive[node+CANT_NIVELES*2]);
+            ++node;
+
+            xQueueSendToBack( humData_queue, ( void * ) &temp_silo[CANT_SILOS][i], ( TickType_t ) 1000 );
+            xQueueSendToBack( tempData_queue, ( void * ) &hum_silo[CANT_SILOS][i], ( TickType_t ) 1000 );
         }
 
         vTaskDelay(NODE_TASK_DELAY / portTICK_PERIOD_MS);
